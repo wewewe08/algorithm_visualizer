e7f8b0b0eb0ddb23a3e73e6002d940476249c43b
yvonne <yvonnehuang0810@gmail.com>
2023-12-02 18:12:41 -0500

    update values

--- build-files/header-files/SortingAlgorithm.h
+++ build-files/header-files/SortingAlgorithm.h
@@ -11,14 +11,14 @@ class SortingAlgorithm {
         SortingAlgorithm(sf::RenderWindow& window, std::vector<sf::RectangleShape>& b, int arrSize) : window(window)
         {
             bars = b;
-            barWidth =10;
-            spacing = 5;
+            barWidth = 8;
+            spacing = 1;
             totalWidth =arrSize * barWidth + (arrSize - 1) * spacing;
             startX = (window.getSize().x - totalWidth) / 2.0f;
         }
 
         void RandomizeArray(int arrSize, std::mt19937 &rng) {
-            std::uniform_int_distribution<std::mt19937::result_type> distribution(50, 300); // this produces random integers within the given range
+            std::uniform_int_distribution<std::mt19937::result_type> distribution(50, 200); // this produces random integers within the given range
 
             for (int i = 0; i < arrSize; ++i) {
                 sf::RectangleShape bar(sf::Vector2f(barWidth, distribution(rng)));
