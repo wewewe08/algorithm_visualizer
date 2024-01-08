c3288c7a6d0fb41c186f28ea83494094b7a1feb2
yvonne <yvonnehuang0810@gmail.com>
2024-01-08 17:16:40 -0500

    FIXED COLOR MIXING BUG

--- build-files/header-files/SelectionSort.h
+++ build-files/header-files/SelectionSort.h
@@ -21,11 +21,12 @@ class SelectionSort : public WindowManager {
             b.setSize(tempSizeA);
             float barBYPos = (window.getSize().y / 2.0f) - b.getSize().y;
             b.setPosition(startX + minIndex * (barWidth + spacing), barBYPos);
-            b.setFillColor(sf::Color::White);
+            //b.setFillColor(sf::Color::White);
         }
 
         void RunSelectionSort() {
             std::vector<std::thread> colorChangeThreads;
+
             for (int i = 0; i < arrSize - 1; ++i) {
                 int minIndex = i;
                 for (int j = i + 1; j < arrSize; ++j) {
@@ -33,30 +34,32 @@ class SelectionSort : public WindowManager {
                     if (WindowManager::bars[j].getSize().y < WindowManager::bars[minIndex].getSize().y) {
                         WindowManager::ChangeColors(WindowManager::bars[minIndex], sf::Color::White); 
                         minIndex = j;
+                        // thread  to change the new minIndex bar to blue while the comparing bar is still updating
                         colorChangeThreads.emplace_back([&](){
-                            WindowManager::ChangeColors(WindowManager::bars[minIndex], minIndexColor); // change color to new minIndex
+                            WindowManager::ChangeColors(WindowManager::bars[minIndex], minIndexColor);
                         });
                     }
                     WindowManager::ResetThreads(colorChangeThreads);
                     WindowManager::ResetWindow();
 
-                    // dont reset minIndex color bar
+                    // resetting comparing bars
                     if (WindowManager::bars[j].getFillColor() != minIndexColor) {
                         WindowManager::ChangeColors(WindowManager::bars[j], sf::Color::White);
                     }
                 }
                 // swapping elements
+                WindowManager::ChangeColors(WindowManager::bars[minIndex], sf::Color::White); 
                 SwapBars(WindowManager::bars[i], WindowManager::bars[minIndex], i, minIndex);
                 WindowManager::ResetWindow();
             }
-            // change last bar color
+            // change last bar color to green
             WindowManager::ChangeColors(WindowManager::bars[arrSize - 1], sortedColor);
             std::cout << "Finished selection sort." << std::endl;
         }
 
     private:
         sf::RenderWindow& window;
-        sf::Color comparingColor = sf::Color(197, 54, 69);
-        sf::Color sortedColor = sf::Color(65,117,113);
-        sf::Color minIndexColor = sf::Color(90, 148, 255);
+        sf::Color comparingColor = sf::Color(197, 54, 69); // red
+        sf::Color sortedColor = sf::Color(65,117,113); // green
+        sf::Color minIndexColor = sf::Color(90, 148, 255); // blue
 };
