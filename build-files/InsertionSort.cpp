405db08a506f6b46a539c674948d1acb6cc66913
yvonne <yvonnehuang0810@gmail.com>
2024-01-13 20:27:46 -0500

    test

--- build-files/InsertionSort.cpp
+++ build-files/InsertionSort.cpp
@@ -11,16 +11,15 @@ InsertionSort::InsertionSort(sf::RenderWindow& window, std::vector<sf::Rectangle
 }
 
 void InsertionSort::RunInsertionSort() {
-    // implementation here 
-    
     int i, j, key;
     for (i = 1; i < arrSize; ++i){
         key = WindowManager::bars[i].getSize().y;
         j = i - 1;
 
         while (j > 0 && WindowManager::bars[j].getSize().y > key) {
-            WindowManager::bars[j+1] = WindowManager::bars[j];
+            WindowManager::ChangeColors(WindowManager::bars[j+1], WindowManager::comparingColor);
             j = j - 1;
+            WindowManager::bars[j+1] = WindowManager::bars[j];
             WindowManager::ResetWindow();
         }
         WindowManager::bars[j+1] = WindowManager::bars[key];
